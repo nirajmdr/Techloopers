dcac099e770940aa21e5fbcadaa76f362718305e
nirajmdr <niraj_mdr@outlook.com>
2020-05-24 13:03:24 +0545

    added vendor

---
+++ app/navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class navigation extends Model
+{
+    protected $table = 'navigations';
+    protected $primaryKey = 'navid';
+}
